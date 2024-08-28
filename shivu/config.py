907a4624464d9afa9097ba25dbbb417664880cb1
Yashdubey213 <ydubey392@gmail.com>
2024-08-28 17:11:47 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
+    OWNER_ID = "6643029630"
+    sudo_users = "6643029630", "7130472271"
+    GROUP_ID =  -1002077799136
+    TOKEN = "7318749803:AAHfbP321t9OtBAGsbY_CO6eUT29DD0Tv-I"
+    mongo_url = "mongodb+srv://mehoca2283:q9unKKrK4gAZvf7U@cluster0.imuhxkw.mongodb.net/?retryWrites=true&w=majority"
+    PHOTO_URL = ["https://telegra.ph/file/b30e42bc296a04a9400af.jpg", "https://telegra.ph/file/b30e42bc296a04a9400af.jpg"]
+    SUPPORT_CHAT = "TALK_2_REL4TE"
+    UPDATE_CHAT = "TALK_2_REL4TE"
+    BOT_USERNAME = "Rio_songbot"
     CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    api_id = 25128421
+    api_hash = "933fd8b53dabcf5c814c47a4f6911623"
 
     
 class Production(Config):
